c33702f0e6f363700b7e923ed643bdfaf16f823c
neeraj <neeraj.bansal@aeris.net>
2023-07-30 10:50:19 +0530

    hapi

--- first.js
+++ first.js
@@ -4,4 +4,14 @@ var addFn = require('./add.js');
 
 var c = addFn.fn1(10,20);
 
-console.log(c);
+
+let timePartitioning = { 'field' : 'abc', 'type':'hour'};
+
+let expiration_time_in_days = '90';
+
+let expiration_time_in_millis = eval(expiration_time_in_days * 24 * 60 * 60 * 1000);
+
+timePartitioning = {...timePartitioning , 'expirationMs' : expiration_time_in_millis.toString()}
+
+console.log(c + ' ' + expiration_time_in_millis);
+console.log(timePartitioning);
